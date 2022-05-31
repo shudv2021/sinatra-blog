d406b0620f11a1a405845f3c9b9bd565cbef759c
dmitry <shudv@yandex.ru>
2022-05-31 09:17:23 +0300

    add view posts

--- app.rb
+++ app.rb
@@ -23,7 +23,8 @@ configure do
 end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	@result = @db.execute ' select * from "Posts" order by id desc'
+erb :index
 end
 
 get '/new_post' do 
